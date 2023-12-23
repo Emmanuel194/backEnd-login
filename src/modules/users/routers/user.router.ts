b5f2f02de01ce1f25d6fa63e89038526d7fb4790
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2023-12-22 21:52:19 -0300

    atualização jwt configuração

--- src/modules/users/routers/user.router.ts
+++ src/modules/users/routers/user.router.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { validateUserCreationMiddleware } from "../middlewares/validate-user-creation.middleware";
-import { validateJwtUser } from "../../../Common/Middlewares/auth.middleware";
 const router = express.Router();
 
 import {
@@ -12,13 +11,11 @@ import {
   authenticate,
 } from "../controllers/user.controller";
 
-router.post("/users", createUser);
+router.post("/users", validateUserCreationMiddleware, createUser);
 router.get("/users", getUsers);
 router.get("/users/:id", getUserByID);
 router.put("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
 router.post("/authenticate", authenticate);
-router.post("/validateUserCreationMiddleware", validateUserCreationMiddleware);
-router.get("/validateJwtUser", validateJwtUser);
 
 export default router;
